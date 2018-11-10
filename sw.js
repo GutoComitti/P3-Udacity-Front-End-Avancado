b1e69be4b03ad61c6d5104124a8ffe635e7b7e99
Augusto Fleith Comitti <gutocomitti@gmail.com>
2018-11-10 10:32:42 -0200

    SW ok, falta responsividade desktop e a11y

--- sw.js
+++ sw.js
@@ -8,10 +8,22 @@ self.addEventListener('install', function(event) {
     caches.open(staticCacheName).then((cache) =>
       cache.addAll([
         '/',
-        'js/main.js',
-        'css/styles.css',
-        'imgs/',
-        'data/restaurants.json'
+        '/index.html',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js'
       ])
     )
   );
